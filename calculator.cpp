c1ee63d6949c9f54c591f279d96f4dafb5478f6a
Yehor <elwhizzkids@gmail.com>
2023-10-17 21:05:36 +0900

    Added new functions which implements division and pow of 2 elements

--- calculator.cpp
+++ calculator.cpp
@@ -15,3 +15,24 @@ int Calculator::Mul (double a, double b)
 {
     return a * b + 0.5;
 }
+
+int Calculator::Div (double a, double b)
+{
+	return a/b+0.5;
+}
+
+int Calculator::Pow(double a, double b)
+{
+    int result = 1;
+    
+    // Перевірка на випадок, якщо b дорівнює 0.
+    if (b == 0) {
+        return 1;
+    }
+    
+    for (int i = 0; i < b; i++) {
+        result *= a;
+    }
+
+    return result;
+}
